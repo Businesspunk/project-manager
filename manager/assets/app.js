4055136f5e0d4253dd843f99b513411652cd623c
Nikita <nikita.ronaldo2014@gmail.com>
2022-04-18 17:50:51 +0300

    Add status and role widgets through twig extension

--- manager/assets/app.js
+++ manager/assets/app.js
@@ -4,7 +4,10 @@ import {Sidebar} from '@coreui/coreui';
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var sidebarToggleButton = document.getElementById('sidebar-toggle');
-    sidebarToggleButton.addEventListener('click', function () {
-        Sidebar.getInstance(document.getElementById('sidebar')).toggle();
-    });
+
+    if (sidebarToggleButton) {
+        sidebarToggleButton.addEventListener('click', function () {
+            Sidebar.getInstance(document.getElementById('sidebar')).toggle();
+        });
+    }
 });
