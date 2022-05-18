b7998e76ccd7ac7c4b10de8c9a31c4eaa5094557
Nikita <nikita.ronaldo2014@gmail.com>
2022-05-18 19:03:26 +0300

    fix: route requirments

--- manager/src/Controller/Work/Projects/Project/ProjectController.php
+++ manager/src/Controller/Work/Projects/Project/ProjectController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller\Work\Projects\Project;
 
+use App\Annotation\GuidAnnotation;
 use App\Model\Work\Entity\Projects\Project\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/work/projects/{id}", name="work.projects.project")
+ * @Route("/work/projects/{id}", name="work.projects.project", requirements={"id"=GuidAnnotation::PATTERN})
  */
 class ProjectController extends AbstractController
 {
