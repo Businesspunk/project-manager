6d4be29e7778e3d3bd03a5f91047421942455c09
Nikita <nikita.ronaldo2014@gmail.com>
2022-06-08 16:48:09 +0300

    feature: add gravatar twig extension

---
+++ manager/src/Twig/Gravatar.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class Gravatar extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('gravatar', [$this, 'getUrl']),
+        ];
+    }
+
+    public function getUrl(string $email, int $size = 80): string
+    {
+        return sprintf('https://www.gravatar.com/avatar/%s%s', md5(strtolower(trim($email))), http_build_query([
+            's' => $size,
+            'd' => 'mp'
+        ]));
+    }
+}
